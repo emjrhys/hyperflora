815d8445613ecf19d235f7295b083ebccebce802
Noah Laferty <noah.lafferty@gmail.com>
2016-05-19 16:43:05 -0500

    simplified gh-pages

--- public/js/home.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $(".content").each(function(i){
-      $(this).mousemove(function(e) {
-          $(".ksc").each(function(i){
-              $(this).css({backgroundPosition: e.pageX+"px "+e.pageY+"px"});
-          });
-      });
-  });
-});
