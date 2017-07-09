28b33e0836660b5154a2a025e6a83a0b022dc812
Noah Laferty <noah.lafferty@gmail.com>
2017-07-09 14:46:32 -0500

    fixed node issues, cleaned up directory

--- app.js
+++ app.js
@@ -1,44 +1,25 @@
 var express  = require('express');
-var path     = require('path');
-var firebase = require('firebase');
+var pug      = require('pug');
 
 var app = express();
-var db = new Firebase('http://hyperflora.firebaseio.com');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
   res.render('home');
 });
 
 app.get('/watch', function (req, res) {
-  db.once('value', function(dataSnapshot) {
-    var videos = [];
-
-    dataSnapshot.forEach(function(child) {
-      videos.push(child.val());
-    });
-
-    console.log(videos);
-    var vid = videos[Math.floor(Math.random() * videos.length)];
-
-    res.render('watch', { video: vid, title: "| " + vid.title });
-  });
-
-  // res.render('watch', { video: { host: "youtube", url: "", title: "Video"} });
+  res.render('watch', { video: vid, title: "| " + vid.title });
 });
 
 app.get('/list', function (req, res) {
-  db.once('value', function(dataSnapshot) {
-    var videos = dataSnapshot.val();
-    console.log(videos);
-    res.render('list', { videos: videos });
-  });
+  res.render('list', { videos: videos });
 });
 
 app.listen(app.get('port'), function() {
